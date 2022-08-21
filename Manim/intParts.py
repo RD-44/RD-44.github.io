f2437b9fd84ba012a38a0ed1de60ba05247a89f8
Aadhavan Mamallan <108578027+RD-44@users.noreply.github.com>
2022-08-21 17:28:07 +0100

    Integration by Parts into
    Added some content and testing out google adsense.

--- Manim/intParts.py
+++ Manim/intParts.py
@@ -28,18 +28,22 @@ class test(Scene):
         self.play(Create(first))
         eq = elem("=", 0.2)
         lhs = [elem("f^\prime(x)", 1, -4, 0),
-                elem("\int f^\prime(x) \,dx", 1.7, -4.5, 0),
-                elem("f(x)", 2, -5, 0),
-                elem("u(x)v(x)", 2, -5, 0),
-                elem("\int u(x)v^\prime(x) \,dx ", 1, -5, 0)]
-        rhs = elem("u^\prime(x)v(x) + u(x)v^\prime(x)", 1)
-        rhs.next_to(lhs[0], RIGHT)
-        eq.next_to(lhs[0], RIGHT)
-        eq.next_to(rhs, LEFT)
-        self.play(Create(lhs[0]), Create(rhs), Create(eq), run_time = 2)
+                elem("\int f^\prime(x) \,dx", 1.7, -4.5, 0), #Sort out heights lol. 
+                elem("f(x)", 1, -5, 0),
+                elem("u(x)v(x)", 1, -5, 0),
+                elem("\int u(x)v^\prime(x) \,dx", 1, -5, 0)]
+        rhs = [elem("u^\prime(x)v(x) + u(x)v^\prime(x)", 1),
+                elem("\int [u^\prime(x)v(x) + u(x)v^\prime(x)] \,dx", 1.5),
+                elem("\int [u^\prime(x)v(x) + u(x)v^\prime(x)] \,dx", 1.5),
+                elem("\int u^\prime(x)v(x) \,dx + \int u(x)v^\prime(x) \,dx", 1.3),
+                elem("u(x)v(x) - \int u^\prime(x)v(x) \,dx", 1.3)]
+        eq.next_to(lhs[0], 2*RIGHT) #Looks promising.
+        rhs[0].next_to(eq, RIGHT)
+        self.play(Create(lhs[0]), Create(rhs[0]), Create(eq), run_time = 2)
         for i in range(1, len(lhs)):
+            rhs[i].next_to(eq, RIGHT)
             self.wait(2)
-            self.play(Transform(lhs[0], lhs[i]), run_time = 1)
+            self.play(Transform(lhs[0], lhs[i]), Transform(rhs[0], rhs[i]), run_time = 1)
         self.wait(2)
 
 
